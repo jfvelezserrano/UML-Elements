08109d3d00aa522e7377e4570d85412e253ee566
Jose Velez <jose.velez@urjc.es>
2022-11-01 10:34:10 +0100

    Renaming some elements, adjusting some sizes, and adding a label to interfaces in Deployment diagrams

--- create_extension.sh
+++ create_extension.sh
@@ -2,4 +2,7 @@
 version=`cat Extension/description.xml | grep 'version value="[0-9][\.0-9]*' | grep -o [0-9][\.0-9]*`
 new_file="UML_Elements-"$version".oxt"
 rm -f $new_file
-zip -r $new_file Extension/*
+cd Extension
+zip -r $new_file ./*
+mv $new_file ../
+cd ..
